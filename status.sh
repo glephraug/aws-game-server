5784534fde4f73953d9d5d4b17fba9bf3cad2a5c
Craig Cambias <craig.cc@gmail.com>
2022-12-23 01:35:15 -0500

    Status now checks all servers

--- status.sh
+++ status.sh
@@ -1,11 +1,9 @@
 
-if [ $1 = "satisfactory" ]
-then
-   sudo systemctl status satisfactory
-fi
-
-if [ $1 = "valheim" ]
-then
-   sudo systemctl status valheim
+if sudo systemctl status satisfactory; then
+   echo Server is running Satisfactory
+elif sudo systemctl status valheim; then
+   echo Server is running Valheim
+else
+   echo Server is not running anything
 fi
 
